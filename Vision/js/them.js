479c1224287db83e1ab415f444b754512c8b060d
hsuanpatty <106578679+hsuanpatty@users.noreply.github.com>
2025-12-18 09:48:37 +0800

    Update them.js

--- Vision/js/them.js
+++ Vision/js/them.js
@@ -1,19 +1,11 @@
-// header banner
 $(".banner-img").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: false,       // 左右箭頭
-  dots: false,         // 頁籤
-  infinite: true,      // loop
+  arrows: false,
+  dots: false,
+  infinite: true,
   autoplay: true,
-  autoplaySpeed: 3000,
-  responsive: [
-    {
-      breakpoint: 576,   // max-width: 575px 以下
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+  autoplaySpeed: 2000,
+  fade: true,     
+  cssEase: 'linear'
 });
