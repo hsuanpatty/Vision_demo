88cbc1da9a3d7fe262a90c6b65a6d7c8516ab273
hsuanpatty <106578679+hsuanpatty@users.noreply.github.com>
2025-12-18 11:43:56 +0800

    Add files via upload

---
+++ Vision/js/them.js
@@ -0,0 +1,11 @@
+$(".banner-img").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  fade: true,     
+  cssEase: 'linear'
+});
